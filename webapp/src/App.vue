955936737a2dd90f54d6e98c37ea5f16827348a6
Daniel Welsh <d.welsh@newcastle.ac.uk>
2019-01-25 14:24:56 +0100

    :sparkles: Adding authentication

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,14 +1,20 @@
 <template lang="pug">
   div#app
     Title
+    Register
+    Login
 </template>
 
 <script>
 import Title from './components/Title.vue';
+import Register from './components/auth/Register.vue';
+import Login from './components/auth/Login.vue';
 
 export default {
   name: 'app',
   components: {
+    Login,
+    Register,
     Title,
   },
 };
---
+++ webapp/src/components/auth/Login.vue
@@ -0,0 +1,75 @@
+<template lang="pug">
+  div#login
+    div.container
+      h1.is-size-3 Login
+      div.field
+        label.label Username
+        div.control.has-icons-left.has-icons-right
+          input.input(
+          type="text",
+          placeholder="johnsmith"
+          v-model="username"
+          )
+          span.icon.is-small.is-left
+            i.fas.fa-user
+
+      div.field
+        label.label Password
+        div.control.has-icons-left.has-icons-right
+          input.input(
+          type="password",
+          placeholder="secret",
+          v-model="password"
+          )
+          span.icon.is-small.is-left
+            i.fas.fa-lock
+
+      div.field
+        div.control
+          button.button.is-link(
+          @click="authenticate"
+          ) Submit
+</template>
+
+<script>
+import API from '../../api';
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    async authenticate() {
+      console.log('authenticating user');
+
+      if (this.username.length === 0 || this.password.length === 0) throw new Error('Username or password cannot be empty.');
+
+      let token;
+      try {
+        token = await API.authenticate(this.username, this.password);
+      } catch (e) {
+        alert('User could not be authenticated');
+      }
+
+      if (token) {
+        this.setCookie(token);
+      }
+
+      console.log(token);
+    },
+    setCookie(value) {
+      const expires = new Date();
+      expires.setDate(expires.getDate() + 1);
+      document.cookie = `${encodeURIComponent('authToken')}=${`${encodeURIComponent(value)}; expires=${expires}`}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
