54994fb65320c95cec56a1ee2a8d7dd550f468f2
Daniel Welsh <d.welsh@newcastle.ac.uk>
2019-01-25 13:18:59 +0100

    :sparkles: Adding temporary landing page

---
+++ webapp/src/components/Title.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div#title
+    section.hero.is-fullheight.is-light.has-text-centered
+      div.hero-body
+        div.container
+          h1.title Ongoingness
+          h2.subtitle Coming soon...
+</template>
+
+<script>
+export default {
+  name: 'Title',
+};
+</script>
+
+<style scoped>
+h1.title {
+  text-transform: uppercase;
+  letter-spacing: 4px;
+  text-align: center;
+}
+</style>
