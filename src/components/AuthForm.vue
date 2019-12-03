d2da98b0095514730927c5d916bea288d128a2b0
Luis Carvalho <lapc19@hotmail.com>
2019-12-03 15:12:35 +0000

    removed register button

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -2,45 +2,18 @@
   div#auth-form
     div.container
       Login(
-        v-if="hasAccount",
         v-on:authenticated="onAuthenticated"
       )
-      Register(
-        v-else,
-        v-on:authenticated="onAuthenticated"
-      )
-      div(
-      v-if="hasAccount"
-      )
-        p.switch Don't have an account?
-        |
-        a(
-        @click="hasAccount = false"
-        ) Register
-      div(
-        v-else
-      )
-        p.switch Have an account?
-        |
-        a(
-        @click="hasAccount = true"
-        ) Sign in
 </template>
 
 <script>
-import Register from './auth/Register.vue';
 import Login from './auth/Login.vue';
 import API from '../api';
 import NotificationController from '../controllers/notification';
 
 export default {
   name: 'AuthForm',
-  components: { Login, Register },
-  data() {
-    return {
-      hasAccount: true,
-    };
-  },
+  components: { Login },
   methods: {
     async onAuthenticated() {
       try {
@@ -61,8 +34,5 @@ export default {
 #auth-form {
   padding-top: 5%;
   padding-bottom: 5%;
-}
-.switch {
-  margin-top: 25px;
 }
 </style>
